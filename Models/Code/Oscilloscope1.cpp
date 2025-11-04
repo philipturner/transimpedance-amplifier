63ca73ef39fda91b3f656cf4b02f6ee359957133
Philip Turner <philipturner.AR@gmail.com>
2025-11-04 13:37:18 -0500

    Upload progress on Oscilloscope1

---
+++ Models/Code/Oscilloscope1.cpp
@@ -0,0 +1,25 @@
+// High-fidelity timer for Teensy
+
+// Displaying data to console
+// - Measure timestamps, auto-detect skipped frames. Fill in skipped frames with
+//   previous sensor value.
+// - Neatly format the time and measured voltage, with a consistent spacing.
+//   Display everything as text to the console, as we cannot display a UI
+//   graph (unfortunately).
+// - The exact subregion of the data stream to display is a non-trivial
+//   problem, even for physical oscilloscopes.
+// - Program two modes: text (high resolution), graphical (low resolution)
+//
+// Exploring the "Serial Plotter" tool right now.
+
+////////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
